5faa10f6cfa55f55db5b9d7584859bb2dc3d0379
AgicaGall <gall_agica01@yahoo.com>
2022-03-20 17:59:33 +0200

    modositasok

--- projekt_szerver.py
+++ projekt_szerver.py
@@ -11,6 +11,15 @@ def main():
     def create_folder():
         source_path = filedialog.askdirectory(title='Select Title')
         path = os.path.join(source_path,input_variable)
+        if(os.path.exists(path)):
+            error_window=Tk()
+            lbl_error = Label(error_window,text="Mar letezik ez az adatbazis!", fg='red', font=('Times New Roman', 16), bg="lightblue")
+            lbl_error.place(x=10,y=50)
+            error_window.title('Error')
+            error_window.geometry("400x100+10+20")
+            error_window.lift()
+            error_window.mainloop()
+            return
         os.makedirs(path)
 
     def drop_folder():
@@ -20,6 +29,15 @@ def main():
     def create_table():
         source_path = filedialog.askdirectory(title='Select Title')
         tabla=source_path+"\\"+tabla_nev+".json"
+        if(os.path.exists(tabla)):
+            error_window=Tk()
+            lbl_error = Label(error_window,text="Mar letezik ez a tabla az adatbazisban!", fg='red', font=('Times New Roman', 16), bg="lightblue")
+            lbl_error.place(x=10,y=50)
+            error_window.title('Error')
+            error_window.geometry("400x100+10+20")
+            error_window.lift()
+            error_window.mainloop()
+            return
         f=open(tabla,'a')
         f.write("[\n]")
         f.close()
@@ -27,6 +45,37 @@ def main():
     def create_column():
         source_path = filedialog.askopenfilename(title='Select Title')
         f=open(source_path,'a+')
+
+        okes=0
+        if(oszlop_index=='foreign-key'):
+            referencia = filedialog.askopenfilename(title='Select reference')
+            while(source_path==referencia):
+                error_window=Tk()
+                lbl_error = Label(error_window,text="Sajat magara nem mutatahat!", fg='red', font=('Times New Roman', 16), bg="lightblue")
+                lbl_error.place(x=10,y=50)
+                error_window.title('Error')
+                error_window.geometry("400x100+10+20")
+                error_window.lift()
+                error_window.mainloop()
+                referencia = filedialog.askopenfilename(title='Select reference')
+            with open(referencia, 'r') as g:
+                data=json.load(g)
+            for i in data:
+                if(i['column role']=='primary-key'):
+                    okes=1
+                    break
+                else:
+                    okes=0
+            if(okes==0):
+                error_window=Tk()
+                lbl_error = Label(error_window,text="Ebben a tablaban nincs primary key!", fg='red', font=('Times New Roman', 16), bg="lightblue")
+                lbl_error.place(x=10,y=50)
+                error_window.title('Error')
+                error_window.geometry("400x100+10+20")
+                error_window.lift()
+                error_window.mainloop()
+                create_column()
+
         size=f.tell()
         f.truncate(size-1)
         f.seek(0, 2)
@@ -36,8 +85,11 @@ def main():
         if karakter[0] != '[':
             f.write(",")
             f.write("\n")
-        
-        data={"column name":oszlop_nev,"column type":oszlop_tipus,"column role":oszlop_index}
+
+        if(oszlop_index=='foreign-key'):         
+            data={"column name":oszlop_nev,"column type":oszlop_tipus,"column role":oszlop_index,"references":referencia}
+        else:
+            data={"column name":oszlop_nev,"column type":oszlop_tipus,"column role":oszlop_index}
         json.dump(data,f,indent=4)
         f.write("\n]")
         f.close()
